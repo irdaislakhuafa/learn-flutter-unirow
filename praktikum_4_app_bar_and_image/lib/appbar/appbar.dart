c44c63468950742cdf3b2a0838ac0b8097f1a86f
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-11-15 12:15:47 +0700

    add app bar

--- praktikum_4_app_bar_and_image/lib/appbar/appbar.dart
+++ praktikum_4_app_bar_and_image/lib/appbar/appbar.dart
@@ -1,3 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:praktikum_4_app_bar_and_image/conf.dart';
+
 void main(List<String> args) {
-	
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: Conf.title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(Conf.title),
+          backgroundColor: Colors.black,
+        ),
+      ),
+    );
+  }
 }
