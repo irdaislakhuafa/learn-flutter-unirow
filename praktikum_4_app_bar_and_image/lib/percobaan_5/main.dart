0519c8dfbc7c137c915ae58dcd5e2dcf4d125dc4
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-11-15 19:55:14 +0700

    add percobaan 5

--- praktikum_4_app_bar_and_image/lib/percobaan_5/main.dart
+++ praktikum_4_app_bar_and_image/lib/percobaan_5/main.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:praktikum_4_app_bar_and_image/conf.dart';
@@ -25,6 +26,21 @@ class App extends StatelessWidget {
               ),
             ),
           ),
+        ),
+        body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: 0, left: 50, right: 50),
+              child: CachedNetworkImage(
+                imageUrl:
+                    "https://ichef.bbci.co.uk/news/976/cpsprodpb/4B45/production/_117796291_gettyimages-962098266.jpg",
+                width: 300,
+                height: 200,
+                placeholder: (context, url) => const Text("gambar anu"),
+                errorWidget: (context, url, error) => Text("error: $error"),
+              ),
+            ),
+          ],
         ),
       ),
     );
