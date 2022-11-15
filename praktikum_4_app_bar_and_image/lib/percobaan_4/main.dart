682edcda30ef758b82eda94b890e876ff5f0488f
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-11-15 19:32:49 +0700

    write code for percobaan 4

--- praktikum_4_app_bar_and_image/lib/percobaan_4/main.dart
+++ praktikum_4_app_bar_and_image/lib/percobaan_4/main.dart
@@ -1,10 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:praktikum_4_app_bar_and_image/conf.dart';
 
-void main(List<String> args) {
-  runApp(App());
-}
+void main() => runApp(App());
 
 class App extends StatelessWidget {
   @override
@@ -17,14 +16,49 @@ class App extends StatelessWidget {
           title: Text(Conf.title),
           flexibleSpace: Container(
             decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
                   Colors.blue,
                   Colors.green,
-                ])),
+                ],
+              ),
+            ),
           ),
+        ),
+        body: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(top: 30),
+              child: Center(
+                child: Image.asset("assets/images/robot.png"),
+              ),
+            ),
+            const Text(
+              "Irda Islakhu Afa | From Assets",
+              style: TextStyle(
+                fontSize: 25,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(
+                  top: 50, left: 50, right: 50, bottom: 20),
+              child: const Image(
+                image: CachedNetworkImageProvider(
+                  "https://ichef.bbci.co.uk/news/976/cpsprodpb/4B45/production/_117796291_gettyimages-962098266.jpg",
+                ),
+              ),
+            ),
+            const Center(
+              child: Text(
+                "From CachedNetworkImage",
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
