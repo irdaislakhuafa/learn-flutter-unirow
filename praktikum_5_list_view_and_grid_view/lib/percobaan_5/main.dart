4a21913a37603dc7c64f6a4176084d63858cb895
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-11-20 22:48:06 +0700

    add code for percobaan 5

---
+++ praktikum_5_list_view_and_grid_view/lib/percobaan_5/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:praktikum_5_list_view_and_grid_view/conf.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  static final List<String> character =
+      "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> listView = [
+      GridView.count(
+        scrollDirection: Axis.horizontal,
+        crossAxisCount: 3,
+        children: List.generate(
+          character.length,
+          (index) {
+            return Card(
+              elevation: 50,
+              margin: const EdgeInsets.all(20),
+              child: Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(20),
+                child: Text(
+                  character[index],
+                  style: const TextStyle(
+                    fontSize: 20,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+      const Card(
+        elevation: 80,
+        margin: EdgeInsets.all(80),
+        child: FlutterLogo(),
+      ),
+    ];
+
+    listView.addAll(
+      Iterable.generate(
+        5,
+        (index) => const Card(
+          elevation: 80,
+          margin: EdgeInsets.all(80),
+          child: FlutterLogo(),
+        ),
+      ),
+    );
+
+    return MaterialApp(
+      title: Conf.title,
+      home: Scaffold(
+        appBar: Conf.defaultAppBar,
+        body: GridView(
+          shrinkWrap: true,
+          scrollDirection: Axis.vertical,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 1,
+          ),
+          children: listView,
+        ),
+      ),
+    );
+  }
+}
