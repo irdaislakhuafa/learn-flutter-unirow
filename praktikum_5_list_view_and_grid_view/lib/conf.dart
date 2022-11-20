8daa86b31ea7e20e52144d92e01b24af2014e63a
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-11-20 20:24:12 +0700

    write code for percobaan 1 ListView vertical scrolling

--- praktikum_5_list_view_and_grid_view/lib/conf.dart
+++ praktikum_5_list_view_and_grid_view/lib/conf.dart
@@ -1,3 +1,5 @@
 class Conf {
   static String title = "Irda Islakhu Afa | Modul 5";
+  static String dummyText =
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ducimus quod deserunt? Neque libero, quia architecto asperiores et minus doloremque eligendi. Architecto commodi dolore, nisi iusto dicta provident quia iste.";
 }
--- praktikum_5_list_view_and_grid_view/lib/percobaan_1/main.dart
+++ praktikum_5_list_view_and_grid_view/lib/percobaan_1/main.dart
@@ -1,9 +1,13 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:praktikum_5_list_view_and_grid_view/conf.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,9 +16,48 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text(Conf.title),
           centerTitle: true,
+          elevation: 0,
         ),
-        body: Text("hello world"),
+        body: ListView(
+          children: [
+            Card(
+              elevation: 50,
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                child: const Text(
+                  "Flutter Widget: Penggunaan ListView Class",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: const EdgeInsets.all(15),
+              elevation: 50,
+              child: Container(
+                padding: const EdgeInsets.all(15),
+                child: Text(
+                  repeat(text: Conf.dummyText, value: 3),
+                  style: const TextStyle(
+                    fontSize: 25,
+                  ),
+                  textAlign: TextAlign.justify,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  static String repeat({int? value, String? text}) {
+    String result = text ?? "";
+    for (int i = 0; i < (value ?? 0); i++) {
+      result += result;
+    }
+    return result;
   }
 }
