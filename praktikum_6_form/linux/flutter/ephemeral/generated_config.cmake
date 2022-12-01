22b78cfef944673bb26a977338be3a32953985fa
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-12-01 15:41:33 +0700

    modify something

---
+++ praktikum_6_form/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/sdks/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/files/Flutter/praktikum_6_form" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/sdks/flutter"
+  "PROJECT_DIR=/files/Flutter/praktikum_6_form"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/files/Flutter/praktikum_6_form/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/files/Flutter/praktikum_6_form/lib/learn/state/main.dart"
+)
