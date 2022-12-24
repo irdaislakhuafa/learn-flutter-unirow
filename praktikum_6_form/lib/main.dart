0346d3373a8e9e256850b7f0dd8c594e1dc5c4d0
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-12-24 19:42:11 +0700

    add hobbies

--- praktikum_6_form/lib/main.dart
+++ praktikum_6_form/lib/main.dart
@@ -16,7 +16,11 @@ class App extends StatefulWidget {
 class _App extends State<App> {
   final _formKey = GlobalKey<FormState>();
 
-  var isMale = true;
+  var _isMale = true;
+  var _checkBoxCoding = false;
+  var _checkBoxSleep = false;
+  var _checkBoxEat = false;
+  var _checkBoxOthers = false;
 
   @override
   Widget build(BuildContext context) {
@@ -35,13 +39,28 @@ class _App extends State<App> {
     );
 
     var npm = Container(
-      child: input(labelText: "  NPM"),
+      child: input(
+        labelText: "   NPM",
+        keyType: TextInputType.number,
+        validator: (v) {
+          if (v == null || v.isEmpty) {
+            return "NPM cannot be null";
+          }
+          return null;
+        },
+      ),
     );
 
     var name = Container(
       child: input(
-        labelText: "  Name",
+        labelText: "   Name",
         icon: Icons.perm_identity_outlined,
+        validator: (v) {
+          if (v == null || v.isEmpty) {
+            return "Name cannot be null";
+          }
+          return null;
+        },
       ),
     );
 
@@ -58,9 +77,9 @@ class _App extends State<App> {
               ),
             ),
           ),
-          value: isMale,
+          value: _isMale,
           onChanged: (value) => setState(() {
-            isMale = value;
+            _isMale = value;
           }),
           secondary: const Icon(
             Icons.male_outlined,
@@ -73,11 +92,110 @@ class _App extends State<App> {
     var address = Container(
       child: input(
         maxLines: 3,
-        labelText: "Address",
+        labelText: "   Address",
         icon: Icons.location_history,
+        validator: (v) {
+          if (v == null || v.isEmpty) {
+            return "Address cannot be null";
+          }
+          return null;
+        },
       ),
     );
 
+    var hobbies = Container(
+      alignment: Alignment.centerLeft,
+      child: cardWithShadow(
+        child: Column(
+          children: [
+            Row(
+              children: const [
+                Icon(
+                  Icons.local_activity_outlined,
+                  color: Colors.blueAccent,
+                ),
+                Text(
+                  "       Hobbies",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.blueAccent,
+                  ),
+                ),
+              ],
+            ),
+            const Divider(),
+            Row(
+              children: [
+                Expanded(
+                  child: CheckboxListTile(
+                    title: const Text("Coding"),
+                    value: _checkBoxCoding,
+                    onChanged: (v) => setState(
+                      () {
+                        _checkBoxCoding = v ?? false;
+                      },
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: CheckboxListTile(
+                    title: const Text("Sleep"),
+                    value: _checkBoxSleep,
+                    onChanged: (v) =>
+                        setState(() => _checkBoxSleep = v ?? false),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: CheckboxListTile(
+                    title: const Text("Eat"),
+                    value: _checkBoxEat,
+                    onChanged: (v) => setState(() => _checkBoxEat = v ?? false),
+                  ),
+                ),
+                Expanded(
+                  child: CheckboxListTile(
+                    title: const Text("Others"),
+                    value: _checkBoxOthers,
+                    onChanged: (v) =>
+                        setState(() => _checkBoxOthers = v ?? false),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+
+    var buttons = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            margin: EdgeInsets.only(right: 20),
+            child: TextButton(
+              onPressed: () => _formKey.currentState!.validate(),
+              child: const Text("Save"),
+              style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all(Colors.white),
+                  backgroundColor:
+                      MaterialStateProperty.all(Colors.blueAccent)),
+            ),
+          ),
+          TextButton(
+            onPressed: () => _formKey.currentState!.reset(),
+            child: const Text("Reset"),
+            style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all(Colors.white),
+                backgroundColor: MaterialStateProperty.all(Colors.blueAccent)),
+          ),
+        ],
+      ),
+    );
     return Scaffold(
       appBar: Conf.appBar,
       body: Form(
@@ -93,6 +211,8 @@ class _App extends State<App> {
                 name,
                 gender,
                 address,
+                hobbies,
+                buttons,
               ],
             ),
           ),
@@ -107,11 +227,16 @@ class _App extends State<App> {
     TextStyle? style,
     IconData? icon,
     int? maxLines,
+    TextInputType? keyType,
+    String? Function(String?)? validator,
   }) {
     return cardWithShadow(
       margin:
           margin ?? EdgeInsets.only(left: 20, right: 20, top: 1, bottom: 10),
       child: TextFormField(
+        validator: validator ??
+            (v) => v == null || v.isEmpty ? "Cannot be null" : null,
+        keyboardType: keyType ?? TextInputType.text,
         maxLines: maxLines ?? 1,
         decoration: InputDecoration(
           label: Text(
