67f8e90ffca56e98b09db862c6df13b6b22ba4f8
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-01-30 02:27:46 +0700

    add settings profile

--- uas/lib/views/settings_profile.dart
+++ uas/lib/views/settings_profile.dart
@@ -12,20 +12,25 @@ class SettingsProfile extends StatefulWidget {
 }
 
 class _SettingsProfileState extends State<SettingsProfile> {
+  final _formKey = GlobalKey<FormState>();
+  String _name = "";
+  String _npm = "";
+  String _password = "";
+
   @override
   Widget build(BuildContext context) {
-    final avatar =
-        Session.getCurrentUser(context).name.substring(0, 1).toUpperCase();
+    final user = Session.getCurrentUser(context);
 
     var items = [
       // avatar
       Container(
         height: 130,
+        margin: EdgeInsets.only(bottom: 50, top: 50),
         child: CircleAvatar(
           backgroundColor: Theme.of(context).primaryColor,
           child: Container(
             child: Text(
-              avatar,
+              user.name.substring(0, 1).toUpperCase(),
               style: Config.defaultTextStyle(
                 context,
                 color: Colors.white,
@@ -38,29 +43,190 @@ class _SettingsProfileState extends State<SettingsProfile> {
 
       // user name
       Container(
+        margin: EdgeInsets.only(left: 30, right: 30),
         child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          elevation: 10,
           child: Container(
             child: Wrap(
               children: [
                 Container(
-                  child: Icon(
-                    Icons.person_outline_rounded,
+                  height: 40,
+                  margin: EdgeInsets.only(top: 10, left: 5),
+                  child: Image.asset("assets/images/person.png"),
+                ),
+                Container(
+                  width: 140,
+                  margin: EdgeInsets.only(top: 2, left: 5),
+                  child: TextFormField(
+                    initialValue: user.name.toUpperCase(),
+                    style: Config.defaultTextStyle(context),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: user.name.toUpperCase(),
+                      hintStyle: Config.defaultTextStyle(context),
+                    ),
+                    onChanged: (value) {
+                      // TODO: add validation here
+                    },
                   ),
                 ),
               ],
             ),
           ),
         ),
-      )
+      ),
+
       // npm
+      Container(
+        margin: EdgeInsets.only(left: 30, right: 30),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          elevation: 10,
+          child: Container(
+            child: Wrap(
+              children: [
+                Container(
+                  height: 40,
+                  margin: EdgeInsets.only(top: 10, left: 5),
+                  child: Image.asset("assets/images/person.png"),
+                ),
+                Container(
+                  width: 140,
+                  margin: EdgeInsets.only(top: 2, left: 5),
+                  child: TextFormField(
+                    initialValue: user.npm.toUpperCase(),
+                    style: Config.defaultTextStyle(context),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: user.npm.toUpperCase(),
+                      hintStyle: Config.defaultTextStyle(context),
+                    ),
+                    onChanged: (value) {
+                      // TODO: add validation here
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+
       // password
+      Container(
+        margin: EdgeInsets.only(left: 30, right: 30),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          elevation: 10,
+          child: Container(
+            child: Wrap(
+              children: [
+                Container(
+                  height: 40,
+                  margin: EdgeInsets.only(top: 10, left: 5),
+                  child: Image.asset("assets/images/person.png"),
+                ),
+                Container(
+                  width: 140,
+                  margin: EdgeInsets.only(top: 2, left: 5),
+                  child: TextFormField(
+                    initialValue: user.password.toUpperCase(),
+                    style: Config.defaultTextStyle(context),
+                    obscureText: true,
+                    obscuringCharacter: "*",
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: user.password.toUpperCase(),
+                      hintStyle: Config.defaultTextStyle(context),
+                    ),
+                    onChanged: (value) {
+                      // TODO: add validation here
+                    },
+                  ),
+                ),
+                // TODO: add button to show password field
+                GestureDetector(
+                  child: Container(
+                    child: Container(
+                      margin: EdgeInsets.only(top: 12, left: 10),
+                      child: Icon(
+                        Icons.remove_red_eye,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                  ),
+                  onTap: () {
+                    // TODO: show password field
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     ];
 
     return Scaffold(
-      body: Container(
-        child: ListView(
-          children: items,
+      body: Form(
+        key: this._formKey,
+        child: Container(
+          child: ListView(
+            children: items,
+          ),
         ),
+      ),
+      bottomNavigationBar: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          // save
+          Container(
+            margin: EdgeInsets.all(20),
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all(Theme.of(context).primaryColor),
+              ),
+              onPressed: () {
+                // TODO: save here
+              },
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  "Simpan",
+                  style: Config.defaultTextStyle(context, color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+
+          // cancel
+          Container(
+            margin: EdgeInsets.all(20),
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all(Theme.of(context).primaryColor),
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  "Batal",
+                  style: Config.defaultTextStyle(context, color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
